4e1bcf949a956a1b6c5ddef7d7c545944e220cff
piyush1017 <70310460+piyush1017@users.noreply.github.com>
2023-04-07 08:19:28 +0530

    Update bodmas.java

--- Project Test1/src/Bodmas/bodmas.java
+++ Project Test1/src/Bodmas/bodmas.java
@@ -5,6 +5,7 @@ public class bodmas {
 	public static void main(String[] args) {
 		
 		int result = (4+10)*3-8/2; // Add 10in palce of 6
+		// Check the answer its show right or not
 		
 		System.out.println(result);
 	}
